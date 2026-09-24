1dd26f6e35daae88fab3ecb87a710f6cd13de69a
Taranjyot Singh <158472433+THEECECODER@users.noreply.github.com>
2026-09-24 21:25:12 +0530

    feat: initialize persistent Chakra color mode

--- frontend-starter/src/app.js
+++ frontend-starter/src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ChakraProvider,Box} from '@chakra-ui/react';
+import {ChakraProvider,Box,ColorModeScript} from '@chakra-ui/react';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import theme from './theme';
 import Layout from './components/Layout';
@@ -11,5 +11,5 @@ import Templates from './pages/Templates';
 import {WebSocketProvider} from './context/WebSocketContext';
 import {AppDataProvider} from './context/AppDataContext';
 
-function App(){return <ChakraProvider theme={theme}><WebSocketProvider><AppDataProvider><Router><Box minH="100vh"><Layout><Routes><Route path="/" element={<Dashboard/>}/><Route path="/conversations" element={<ConversationView/>}/><Route path="/conversation/:id" element={<ConversationView/>}/><Route path="/agent-config" element={<AgentConfig/>}/><Route path="/templates" element={<Templates/>}/><Route path="/Analysis" element={<Analysis/>}/></Routes></Layout></Box></Router></AppDataProvider></WebSocketProvider></ChakraProvider>}
+function App(){return <ChakraProvider theme={theme}><ColorModeScript initialColorMode={theme.config.initialColorMode}/><WebSocketProvider><AppDataProvider><Router><Box minH="100vh"><Layout><Routes><Route path="/" element={<Dashboard/>}/><Route path="/conversations" element={<ConversationView/>}/><Route path="/conversation/:id" element={<ConversationView/>}/><Route path="/agent-config" element={<AgentConfig/>}/><Route path="/templates" element={<Templates/>}/><Route path="/Analysis" element={<Analysis/>}/></Routes></Layout></Box></Router></AppDataProvider></WebSocketProvider></ChakraProvider>}
 export default App;
