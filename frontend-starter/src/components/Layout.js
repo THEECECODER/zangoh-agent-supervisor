9729b0492fb16605f3253456d15f8510381f73d3
Taranjyot Singh <158472433+THEECECODER@users.noreply.github.com>
2026-09-24 19:59:05 +0530

    ui: refresh Layout.js

--- frontend-starter/src/components/Layout.js
+++ frontend-starter/src/components/Layout.js
@@ -1,27 +1,15 @@
-// src/components/Layout.js
 import React from 'react';
-import { Box, Flex, useColorMode } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Sidebar from './Sidebar';
 import Header from './Header';
 
-const Layout = ({ children }) => {
-  const { colorMode } = useColorMode();
-  
-  return (
-    <Flex direction="row" minHeight="100vh">
-      <Sidebar />
-      <Box
-        flex="1"
-        bg={colorMode === 'dark' ? 'gray.800' : 'gray.50'}
-        minH="100vh"
-      >
-        <Header />
-        <Box as="main" p={4} ml={{ base: 0, md: '250px' }}>
-          {children}
-        </Box>
-      </Box>
-    </Flex>
-  );
-};
-
+const Layout = ({ children }) => (
+  <Box minH="100vh" bg="canvas">
+    <Header />
+    <Sidebar />
+    <Box as="main" pt="76px" pl={{base:0,md:'206px'}} minH="100vh">
+      <Box px={{base:4,md:7}} py={5} maxW="1500px" mx="auto">{children}</Box>
+    </Box>
+  </Box>
+);
 export default Layout;
